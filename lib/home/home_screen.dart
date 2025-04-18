eef7c926fc5abbe948216c525b633afbbee99505
TheHCJ <150591975+TheHCJ@users.noreply.github.com>
2025-04-18 11:44:08 +0100

    fix copilot breaking pageview in home_screen

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -200,7 +200,7 @@ class _ScaffoldWithBottomNavigationState extends State<ScaffoldWithBottomNavigat
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(..........................................................................................................................................................................................................................................................allowImplicitScrolling: ,
+      body: PageView(
         controller: _pageController,
         onPageChanged: (page) {
           setState(() {
